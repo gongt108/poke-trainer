5196071e565b51e8066c01168a42e7b0e9ac58dd
Tiffany Gong <gongt108@gmail.com>
2023-10-03 01:50:49 -0700

    feat: added index.html, style.css, app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+// Get variables
+const gameContainer = document.querySelector('#game-container');
+const gameTitle = document.querySelector('#game-title');
+const startBtn = document.querySelector('#start-btn');
